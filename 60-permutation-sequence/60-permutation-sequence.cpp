84273648b912995e05707587020ea1fd1da87789
TheIncredibleVee <39600438+TheIncredibleVee@users.noreply.github.com>
2022-07-28 00:48:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (80.63%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void factorial(vector<int> &dp, int n){
+        dp[0] = 1;
+        for(int i=1;i<=n;i++){
+            dp[i] = dp[i-1]*i;
+        }
+    }
+    string getPermutation(int n, int k) {
+        string s = "";
+        for(int i = 0; i < n; ++i){
+            s+=to_string(i+1);
+        }
+        string res = "";
+        k-=1;
+        vector<int> dp(n+1);
+        factorial(dp, n);
+        while(k > 0 && n > 0){
+            if(k < dp[n-1]){
+                res += s[0];
+                s = s.substr(1);
+            }else{
+                res += s[k/dp[n-1]];
+                s.erase(s.begin() + k/dp[n-1]);
+                k %= dp[n-1];
+            }
+            n--;
+        }
+        if(!s.empty()){
+            res+=s;
+        }
+        return res;
+    }
+};
