99701d608f865dd53a81adf47e4145c0ac51c951
TheIncredibleVee <39600438+TheIncredibleVee@users.noreply.github.com>
2022-08-13 18:30:44 +0530

    Time: 78 ms (84.57%), Space: 31.2 MB (49.06%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,5 +1,34 @@
 class Solution {
 private:
+    void func(string s, unordered_map<string, int> & mp, int wordSize, int totalWords, vector<int> & res, int offset){
+        unordered_map<string, int> mp2;
+        string temp = "";
+        int n = s.length();
+        int i = 0;
+        for(i = 0; i < totalWords; ++i){
+            temp = s.substr(i*wordSize, wordSize);
+            // cout<<temp<<endl;
+            mp2[temp]++;
+        }
+        int end = totalWords * wordSize;
+        int j;
+        for( j = wordSize; j + end - 1 < n ; j += wordSize){
+            if(mp2 == mp){
+                res.push_back(j - wordSize + offset );
+            }
+            temp = s.substr(j - wordSize, wordSize);
+            mp2[temp]--; 
+            if(mp2[temp] == 0){
+                mp2.erase(temp);
+            }
+            temp = s.substr(j + end - wordSize, wordSize);
+            mp2[temp]++;
+        }
+        if(mp2 == mp){
+            res.push_back(j - wordSize + offset);
+        }
+    }
+    
     int getNextIndex(string s, unordered_map<string, int> &mp, int wordSize, int n){
         unordered_map<string, int> mp2 = mp;
         string temp = "";
@@ -43,16 +72,19 @@ public:
         int n = s.length();
         int i;
         int totalSize = wordsN * wordSize;
-        for(int i = 0; i< (n - totalSize + 1); i++){
-            string temp = s.substr(i, totalSize);
-            // int nextIdx = getNextIndex(temp, mp, wordSize, totalSize);
-            // if(nextIdx == totalSize){
-            //     res.push_back(i);
-            // }
-            // i += nextIdx;
-            if(check(temp, mp, wordSize, totalSize)){
-                res.push_back(i);
-            }
+        // for(int i = 0; i< (n - totalSize + 1); i++){
+        //     string temp = s.substr(i, totalSize);
+        //     // int nextIdx = getNextIndex(temp, mp, wordSize, totalSize);
+        //     // if(nextIdx == totalSize){
+        //     //     res.push_back(i);
+        //     // }
+        //     // i += nextIdx;
+        //     if(check(temp, mp, wordSize, totalSize)){
+        //         res.push_back(i);
+        //     }
+        // }
+        for(int i = 0; i < wordSize; ++i){
+            func(s.substr(i), mp, wordSize, wordsN, res, i);
         }
         return res;
     }
